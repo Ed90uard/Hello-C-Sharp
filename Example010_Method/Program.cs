cd6d84bb94af36759c5b666814fbc3f8269fbf48
Ed Kazaryan <st.edie@mail.ru>
2022-02-26 14:54:58 +0300

    010

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 11, 22, 13, 44, 52, 65, 17, 81 };
+
+int n = array.Length;
+int find = 2;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
+
